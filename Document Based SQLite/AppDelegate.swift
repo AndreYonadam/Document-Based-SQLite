d4f2fbde8607e5b38792c29b1cdc1dea5cb97811
Andre Yonadam <agy2154@gmail.com>
2018-06-19 19:46:38 -0500

    Initial Commit

---
+++ Document Based SQLite/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Document Based SQLite
+//
+//  Created by Andre Yonadam on 6/19/18.
+//  Copyright © 2018 Andre Yonadam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Document Based SQLite/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Document Based SQLite
+//
+//  Created by Andre Yonadam on 6/19/18.
+//  Copyright © 2018 Andre Yonadam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
